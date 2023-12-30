a8b751a8320368da2b868b7296c0c19214d66a0a
Maryanne Wanjiru Weru <86960824+Pearl20-20@users.noreply.github.com>
2023-12-30 15:23:22 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,34 +1,59 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
-
+"""
+This module contains a function that returns the HTML content of a particular
+URL and stores it in a Redis cache
+"""
 import redis
 import requests
-from typing import Callable
+from typing import Optional, Callable
 from functools import wraps
 
-redis = redis.Redis()
 
-
-def wrap_requests(fn: Callable) -> Callable:
-    """ Decorator wrapper """
-
-    @wraps(fn)
-    def wrapper(url):
-        """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
-        if cached_response:
-            return cached_response.decode('utf-8')
-        result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
-        return result
-
-    return wrapper
-
-
-@wrap_requests
-def get_page(url: str) -> str:
-    """get page self descriptive
+def count_url_calls(fn: Callable) -> Callable:
     """
-    response = requests.get(url)
-    return response.text
+    Wrapper function that counts how many times a URL is called
+
+    Args:
+        fn (Callable): function to wrap
+
+    Returns:
+        Callable: wrapped function
+    """
+    @wraps(fn)
+    def increment_url_count(*args, **kwargs):
+        """
+        Increment calls of method
+        """
+        r: redis.Redis = redis.Redis()
+        url: str = ''
+        if args:
+            url = args[0]
+        if kwargs:
+            url = kwargs["url"] if "url" in kwargs else url
+        r.incr("count:{}".format(url))
+        return fn(*args, **kwargs)
+    return increment_url_count
+
+
+@count_url_calls
+def get_page(url: str) -> str:
+    """
+    Returns the HTML content of the URL
+
+    Args:
+        url (str): URL to request
+
+    Returns:
+        str: HTML content of the URL
+    """
+    r: redis.Redis = redis.Redis()
+
+    page_cache: Optional[bytes] = r.get(url)
+    html_content: Optional[str] = ''
+    if page_cache:
+        html_content = page_cache.decode('utf-8')
+    else:
+        html_content = requests.get(url).text
+        r.setex(url, 10, html_content)
+
+    return html_content
