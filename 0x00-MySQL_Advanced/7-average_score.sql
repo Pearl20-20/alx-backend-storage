0a592c1d9e388f8c2cca36d2186185b3ecf6df29
Maryanne Wanjiru Weru <86960824+Pearl20-20@users.noreply.github.com>
2023-12-30 15:17:24 +0300

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,15 @@
--- Script that creates a stored procedure AddBonus
--- that adds a new correction for a student.
+-- Script that creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student
+drop procedure IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$ ;
-CREATE PROCEDURE AddBonus(
-	IN user_id INTEGER,
-	IN project_name VARCHAR(255),
-	IN score INTEGER
+CREATE PROCEDURE ComputeAverageScoreForUser(
+	IN user_id INT
 )
 BEGIN
-	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
-		INSERT INTO projects (name) VALUES (project_name);
-	END IF;
-	INSERT INTO corrections (user_id, project_id, score)
-	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+	UPDATE users
+   	SET average_score=(SELECT AVG(score) FROM corrections
+			     WHERE corrections.user_id=user_id)
+	WHERE id=user_id;
+
 END;$$
 DELIMITER ;
